13d7f888b276bebaf33332780dc2c286858fae49
Zsolt Nagy <info@zsoltnagy.eu>
2022-11-18 02:58:28 +0100

    Console logging form values

--- src/Components/WorkoutForm/WorkoutForm.js
+++ src/Components/WorkoutForm/WorkoutForm.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
 export default function WorkoutForm() {
+    const activityInput = React.useRef();
+    const quantityInput = React.useRef();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const activityValue = activityInput.current.value;
+        const quantityValue = quantityInput.current.value;
+
+        console.log(activityValue, quantityValue);
+    }
+
     return (
-        <form action="#" method="GET">
+        <form action="#" method="GET" onSubmit={handleSubmit}>
             <div>
                 <label>
                     Activity:
-                    <input type="text" name="activity" />    
+                    <input 
+                        type="text" 
+                        name="activity"
+                        ref={activityInput} />    
                 </label> 
             </div>
             <div>
                 <label>
                     Quantity:
-                    <input type="text" name="quantity" /> 
+                    <input 
+                        type="text" 
+                        name="quantity"
+                        ref={quantityInput} /> 
                 </label>
             </div>
             <button type="submit">Add</button>
